065317d4c05b994902e07bca1639aa3fe06b303d
Sreehari2001 <141338988+NewYearBoy2001@users.noreply.github.com>
2026-05-20 12:39:20 +0530

    updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps  // Add this only if using google_maps_flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // Add this only if using google_maps_flutter
+    GMSServices.provideAPIKey("AIzaSyAnKXxYTw3kCdtNo_r6MN4A6rFLl_oWjbs")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
--- lib/app/modules/merchant_home/views/merchant_gallerycontroller.dart
+++ lib/app/modules/merchant_home/views/merchant_gallerycontroller.dart
@@ -24,7 +24,7 @@ class MerchantGalleryController extends GetxController {
   String get token => box.read("auth_token") ?? "";
 
   final String baseUrl =
-      "https://entenaadu.co.in/api/merchant";
+      "https://eshoppy.co.in/api/merchant";
 
   @override
   void onInit() {
